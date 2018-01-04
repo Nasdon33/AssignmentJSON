68a787f0fca2518a634afa0b366683fff05bdc84
Nasdon33 <carlo.1994@live.it>
2018-01-04 16:13:07 +0100

    Aggiunta base per test di assignments.js

---
+++ __tests__/Assignment.test.js
@@ -0,0 +1,20 @@
+const assignments = require('../assignments');
+
+// WIP
+
+
+test('Aggiungo Assignment e poi lo ricevo', () => {
+
+});
+
+test('Aggiungo Assignment e poi lo cancello', () => {
+    
+});
+
+test('Aggiungo Assignment e poi lo modifico', () => {
+    
+});
+
+test('Aggiungo degli Assignment e poi ne ricevo alcuni filtrandoli per Task ID', () => {
+    
+});
